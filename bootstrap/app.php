958493bb76baa812cc55de2a487e3343e1e72e48
happy <happyblackie01@gmail.com>
2024-06-14 13:15:59 -0700

    Basic Laravel 11 user roles and permissions by Funda Of WebIT

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
@@ -9,9 +10,11 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-    )
+    ) 
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'is_Admin' => AdminMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
